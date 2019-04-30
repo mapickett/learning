e48d16a9648cc0dbb62fb12fe789cba044ed849f
mapickett <mapickett77@gmail.com>
2019-04-29 21:34:48 -0400

    .

--- pythonforkids/chap3.py
+++ pythonforkids/chap3.py
@@ -6,3 +6,14 @@ foods = ['pizza', 'burger', 'cake']
 
 print(games + foods)
 
+#2: Counting Combatants
+
+combatants = ((3*25)+ (2*20))
+print('There are %s combatants!' % combatants)
+
+#3: Greetings
+
+first_name = 'McLean'
+last_name = 'Pickett'
+
+print('''"Hi there, %s %s!"''' % (first_name, last_name))
