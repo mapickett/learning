84109dbd88e5c0a447c26e2c9de07cc3a0c44168
mapickett <mapickett77@gmail.com>
2019-07-19 21:09:44 -0400

    commit

---
+++ pythonforkids/chap5.py
@@ -0,0 +1,47 @@
+age = 25
+
+if age > 20:
+    print('You are too old!')
+    print('Why are you here?')
+    print('Why aren\'t you mowing a lawn or sorting papers?')
+    
+age = 10
+if age >= 10:
+    print('You\'re too old for my jokes')
+    
+print('Want to hear a dirty joke?')
+
+age = 12
+if age == 12:
+    print("A pig fell in the mud")
+elif age == 11:
+    print('What did 0 say to 8?')
+    print('Hi guys!')
+else:
+    print("Shh. It's a secret.")
+    
+myval = None
+print(myval)
+
+if myval == None:
+    print('The variable doesn\'t have a value')
+
+string = '127'
+stringint = int(string)
+print(stringint)
+
+stringstring = str(stringint)
+
+money = 2000
+if money > 1000:
+    print('I\'m rich!!')
+else:
+    print('I\'m not rich!!')
+
+twinkies = 0
+
+if twinkies < 100 or twinkies > 500:
+    print('too few or too many')
+
+
+
